a08290ae9c5a348021fe55948fb0b2e45fcfcea7
Otieno Elvis Sia <elvis.otieno2024@students.jkuat.ac.ke>
2025-06-25 21:35:11 +0300

    j

--- links/script.js
+++ links/script.js
@@ -13,6 +13,15 @@ document.addEventListener("DOMContentLoaded", () => {
     })
 });
 
+// Hamburger toggle
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburger = document.getElementById("hamburger");
+    const navLinks = document.getElementById("nav-links");
+
+    hamburger.addEventListener("click", () => {
+        navLinks.classList.toggle("show");
+    });
+});
 
 //Cart.js
 document.addEventListener('DOMContentLoaded', () => {
